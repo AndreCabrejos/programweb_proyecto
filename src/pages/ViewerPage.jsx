987ae9e28cf2c6340714228932970ecd07b28187
Alonso Huamán <alonsohuaman@MacBook-Air-de-Valeria-3.local>
2025-10-14 23:55:55 -0500

    avance2

--- src/pages/ViewerPage.jsx
+++ src/pages/ViewerPage.jsx
@@ -1,12 +1,31 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Regalos from "../components/Regalos";
+import Notificacion from "../components/Notificacion";
 import "./ViewerPage.css";
 
 export default function ViewerPage({ monedas, setMonedas }) {
+
+  const [nivel, setNivel] = useState(1);
+  const [progreso, setProgreso] = useState(0);
+  const [showNotif, setShowNotif] = useState(false);
+  const [mensaje, setMensaje] = useState("");
+
   const handleEnviarRegalo = (regalo) => {
     setMonedas((prev) => prev - regalo.costo);
     console.log(`Regalo enviado: ${regalo.nombre}`);
+    const nuevoProgreso = progreso + regalo.costo;
+    setProgreso(nuevoProgreso);
+    
+    // Cada 100 puntos subes de nivel
+    if (nuevoProgreso >= nivel * 100) {
+      const nuevoNivel = nivel + 1;
+      setNivel(nuevoNivel);
+      setMensaje(`🎉 ¡Has subido al nivel ${nuevoNivel}!`);
+      setShowNotif(true);
+    }
   };
+  
+    
 
   return (
     <div className="viewer-page">
@@ -18,6 +37,22 @@ export default function ViewerPage({ monedas, setMonedas }) {
       <aside className="sidebar-chat">
         <Regalos monedas={monedas} onEnviarRegalo={handleEnviarRegalo} />
       </aside>
+      {/* Notificación solo aparece cuando showNotif es true */}
+      <Notificacion
+        message={mensaje}
+        show={showNotif}
+        onClose={() => setShowNotif(false)}
+      />
+
+      
     </div>
+    
+    
+    
+
+
+    
+
+    
   );
 }
